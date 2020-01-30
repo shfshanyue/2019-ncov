b6859d2f24119bbe910f619bcd4c06ce985450df
shfshanyue <xianger94@gmail.com>
2020-01-30 23:52:44 +0800

    Add 实时动态

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, Suspense, useEffect } from 'react'
+import useSWR from 'swr'
 import keyBy from 'lodash.keyby'
 import dayjs from 'dayjs'
 
@@ -8,15 +9,70 @@ import provinces from './data/area'
 import Tag from './Tag'
 
 import './App.css'
+import axios from 'axios'
 
 const Map = React.lazy(() => import('./Map'))
 
 const provincesByName = keyBy(provinces, 'name')
 const provincesByPinyin = keyBy(provinces, 'pinyin')
 
-function App() {
-  const [province, _setProvince] = useState(null)
+const fetcher = (url) => axios(url).then(data => {
+  return data.data.data
+})
 
+function New ({ title, summary, sourceUrl, pubDate, pubDateStr }) {
+  return (
+    <div className="new">
+      <div className="new-date">
+        <div className="relative">
+          {pubDateStr} 
+        </div>
+        {dayjs(pubDate).format('YYYY-MM-DD HH:mm')}
+      </div>
+      <a className="title" href={sourceUrl}>{ title }</a>
+      <div className="summary">{ summary.slice(0, 100) }...</div>
+    </div>
+  )
+}
+
+function News ({ province }) {
+  const [len, setLen] = useState(8)
+  const { data: news } = useSWR('https://file1.dxycdn.com/2020/0127/794/3393185296027391740-115.json', fetcher, {
+    initialData: []
+  })
+
+  return (
+    <div className="card">
+      <h2>实时动态</h2>
+      {
+        news
+          .filter(n => province ? province.provinceShortName === (n.provinceName && n.provinceName.slice(0, 2)) : true)
+          .slice(0, len)
+          .map(n => <New {...n} key={n.id} />)
+      }
+      <div className="more" onClick={() => { setLen() }}>点击查看全部动态</div>
+    </div>
+  )
+}
+
+function Summary () {
+  return (
+    <div className="card info">
+      <h2>信息汇总</h2>
+      <li>
+        <a href="https://m.yangshipin.cn/static/2020/c0126.html">疫情24小时 | 与疫情赛跑</a>
+      </li>
+      <li><a href="http://2019ncov.nosugartech.com/">确诊患者同行查询工具</a></li>
+      <li><a href="https://news.qq.com/zt2020/page/feiyan.htm">腾讯新闻新冠疫情实时动态</a></li>
+      <li><a href="https://3g.dxy.cn/newh5/view/pneumonia">丁香园新冠疫情实时动态</a></li>
+      <li><a href="https://vp.fact.qq.com/home">新型冠状病毒实时辟谣</a></li>
+      <li><a href="https://promo.guahao.com/topic/pneumonia">微医抗击疫情实时救助</a></li>
+    </div>
+  )
+}
+
+function App () {
+  const [province, _setProvince] = useState(null)
   const setProvinceByUrl = () => {
     const p = window.location.pathname.slice(1)
     _setProvince(p ? provincesByPinyin[p] : null)
@@ -141,17 +197,8 @@ function App() {
         </div>
         { renderArea() }
       </div>
-      <div className="card info">
-        <h2>信息汇总</h2>
-        <li>
-          <a href="https://m.yangshipin.cn/static/2020/c0126.html">疫情24小时 | 与疫情赛跑</a>
-        </li>
-        <li><a href="http://2019ncov.nosugartech.com/">确诊患者同行查询工具</a></li>
-        <li><a href="https://news.qq.com/zt2020/page/feiyan.htm">腾讯新闻新冠疫情实时动态</a></li>
-        <li><a href="https://3g.dxy.cn/newh5/view/pneumonia">丁香园新冠疫情实时动态</a></li>
-        <li><a href="https://vp.fact.qq.com/home">新型冠状病毒实时辟谣</a></li>
-        <li><a href="https://promo.guahao.com/topic/pneumonia">微医抗击疫情实时救助</a></li>
-      </div>
+      <News province={province} />
+      <Summary />
       <div className="fallback">
         <div>
           代码仓库: <a href="https://github.com/shfshanyue/2019-ncov">shfshanyue/2019-ncov</a>
