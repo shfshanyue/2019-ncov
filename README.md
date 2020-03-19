fd9fd984316dce8cb97a4556362679b5b6456b52
Han <han@karma.wiki>
2020-03-19 09:37:01 +0800

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-Test site for convid19 in AU.
-
-[link](https://freddiewanah.github.io/2019-ncov/)
---
+++ precache-manifest.a1f12b299e8cb448d78c199ed62e989a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081f1b69c59e2a05f28ee9b21a3b161b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83d5fb879ff9767d54ed",
+    "url": "/static/css/main.af38c2aa.chunk.css"
+  },
+  {
+    "revision": "fc87bafc5979a1247fd5",
+    "url": "/static/js/4.b91a4e84.chunk.js"
+  },
+  {
+    "revision": "0297a4d98585e468f45f",
+    "url": "/static/js/5.7ec54558.chunk.js"
+  },
+  {
+    "revision": "76cc6ab99dbfae727f95",
+    "url": "/static/js/data.00a1b622.chunk.js"
+  },
+  {
+    "revision": "83d5fb879ff9767d54ed",
+    "url": "/static/js/main.a8e44b54.chunk.js"
+  },
+  {
+    "revision": "f38d542701f485c08aad37761e6b423f",
+    "url": "/static/js/main.a8e44b54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5eb8f3f0fef41f15a1cf",
+    "url": "/static/js/react.739f6217.chunk.js"
+  },
+  {
+    "revision": "9300ecbebf391c6c2038edb15bc7e2cb",
+    "url": "/static/js/react.739f6217.chunk.js.LICENSE"
+  },
+  {
+    "revision": "513cdb7e01dbd4462ac8",
+    "url": "/static/js/runtime-main.9a670a63.js"
+  },
+  {
+    "revision": "d71edfa6fb9ba10de0358502a5a968fd",
+    "url": "/static/media/confirmed.d71edfa6.png"
+  },
+  {
+    "revision": "487fdc083f77ed81c35581dfc03e8bb9",
+    "url": "/static/media/feiyan.487fdc08.jpeg"
+  },
+  {
+    "revision": "b7ca8b8f4c1f410f67a2a01bc73bdd77",
+    "url": "/static/media/hospital.b7ca8b8f.png"
+  }
+]);
